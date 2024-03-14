f2008d16a0b54f4c003699c7637b0ea747d52deb
Andre <85369189+andrbtn@users.noreply.github.com>
2024-03-14 12:27:31 +0100

    add database connection (mongodb)

--- back/app.js
+++ back/app.js
@@ -7,8 +7,12 @@ const logger = require('morgan');
 const indexRouter = require('./api');
 const usersRouter = require('./api/routes/flights');
 const routes = require('../back/api');
-
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
+mongoose.connect(process.env.DB_CONNECTION)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,26 +20,26 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
