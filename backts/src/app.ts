4a8f85afc64c20f7770bcb7edac5e474040108b8
DimitriDR <dimitridroeck@gmail.com>
2024-04-17 21:24:44 +0200

    Improving app shutdown for Docker in WIP
    Signed-off-by: DimitriDR <dimitridroeck@gmail.com>

--- backts/src/app.ts
+++ backts/src/app.ts
@@ -11,19 +11,32 @@ const PORT: number = Number(process.env.PORT) || 4000;
 async function startServer() {
     try {
         await mongoose.connect(String(process.env.DB_CONNECTION)).then(() => {
-            console.log('Connected to MongoDB');
+            console.log('[INFO] Connecté à MongoDB !');
         }).catch((error) => {
-            console.error(`Error connecting to MongoDB: ${error}`);
+            console.error(`[ERREUR] Erreur lors de la connexion à la MongoDB : ${error}`);
         });
         // middleware - before routes
         app.use(express.json());
         app.use(express.urlencoded({extended: false}));
 
-        // routes
+        // Routes
         app.use('/api', routes);
-        // Start the server
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+
+        // Démarrage du serveur
+        const server = app.listen(PORT, (): void => {
+            console.log(`[INFO] Le serveur tourne sur le port ${PORT}`);
+        });
+
+        process.on('SIGINT', () => {
+            console.log(`[INFO] Merci d'avoir évalué l'API SkyHub. L'application se termine…`);
+
+            server.close(() => {
+                mongoose.disconnect().then(() => {
+                    console.log(`[INFO] Déconnecté de MongoDB !`);
+                    console.log(`[INFO] Application terminée.`);
+                    process.exit(0);
+                });
+            });
         });
     } catch (error) {
         console.error(`Error : ${error}`);
