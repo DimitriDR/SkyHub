ef9cb8f7e2837877f7b0f3488bd5a1ac768d46d4
Andre <85369189+andrbtn@users.noreply.github.com>
2024-04-30 15:07:49 +0200

    Add airports verification for flights & fixed id for flights.json

--- back/src/controllers/flights-controller.ts
+++ back/src/controllers/flights-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import {getAll, add, update, deleteFlightFromDB, getById} from "../repositories/flights-mongo-repositories";
+import {Flight} from "../models/flights";
+import Airport from "../models/airports";
 
 export function getFlights(req : Request, res : Response) {
     try {
@@ -19,18 +21,49 @@ export function getFlightById(req : Request, res : Response) {
     }
 }
 
-export function createFlight(req : Request, res : Response) {
+async function checkAirportExists(req : Request) {
+    let newFlight = new Flight(req.body);
+    let airportOk = true;
+    await Airport.exists({
+        _id: newFlight.origin_id
+    }).then((exists) => {
+        if (!exists) {
+            airportOk = false;
+        }
+    });
+
+    await Airport.exists({
+        _id: newFlight.destination_id
+    }).then((exists) => {
+        if (!exists) {
+            airportOk = false;
+        }
+    });
+
+    console.log('Airport ids exist ? ', airportOk);
+    return airportOk;
+}
+
+export async function createFlight(req: Request, res: Response) {
     try {
         console.log('createFlight');
+        const airportOk: boolean = await checkAirportExists(req);
+        if (!airportOk) {
+            return res.status(404).json({message: "L'aéroport de départ ou d'arrivée est invalide."});
+        }
         return add(req, res);
     } catch (e) {
         res.status(500).send("Une erreur est survenue.");
     }
 }
 
-export function updateFight(req : Request, res : Response) {
+export async function updateFight(req : Request, res : Response) {
     try {
         console.log('updateFlight');
+        const airportOk: boolean = await checkAirportExists(req);
+        if (!airportOk) {
+            return res.status(404).json({message: "L'aéroport de départ ou d'arrivée est invalide."});
+        }
         return update(req, res);
     } catch (e) {
         res.status(500).send("Une erreur est survenue.");
